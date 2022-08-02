11364cf802e02fdab1c87439234d8296594c32ca
Eddy <eduardo.di.garcia@gmail.com>
2022-08-01 21:43:14 -0400

    adding class used

---
+++ UsedCarLotLab/Car.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace UsedCarLotLab
+{
+	public class Car
+	{
+
+
+        //Constructor
+        public Car(string make, string model, int year, decimal price)
+		{
+			Make = make;
+			Model = model;
+			Year = year;
+			Price = price;
+
+        }
+        public Car()
+        {
+        }
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+        public decimal Price { get; set; }
+
+
+
+
+
+
+    }
+}
+
---
+++ UsedCarLotLab/UsedCar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace UsedCarLotLab
+{
+	public class UsedCar: Car
+	{
+        //Constructor
+        public UsedCar(string make, string model, int year, decimal price,double mileage)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            Price = price;
+            Mileage = mileage;
+        }
+
+        public double Mileage { get; set; }
+
+
+    }
+}
+
